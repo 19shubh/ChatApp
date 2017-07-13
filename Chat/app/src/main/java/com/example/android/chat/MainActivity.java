423e3b6947df9bae3bd7367517bcf9173e27ebb4
Deepak <deepakkumar180997@gmail.com>
2017-07-13 16:36:51 +0530

    Auth_done

--- Chat/app/src/main/java/com/example/android/chat/MainActivity.java
+++ Chat/app/src/main/java/com/example/android/chat/MainActivity.java
@@ -1,3 +1,40 @@
+package com.example.android.chat;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
     private static final String TAG = "MainActivity";
 
     public static final String ANONYMOUS = "anonymous";
@@ -33,7 +70,7 @@
 
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDataBaseReference = mFirebaseDatabase.getReference().child("message");
-
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         // Initialize references to views
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -68,6 +105,7 @@
 
             }
         });
+        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(DEFAULT_MSG_LENGTH_LIMIT)});
 
         mSendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +116,33 @@
                 mMessageEditText.setText("");
             }
         });
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //signed in
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    //signed out
+                    onSignedOutCleanup();
+                    //providers
+
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+                    );
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(providers)
+                                    .setIsSmartLockEnabled(false)
+                                    .build(), RC_SIGN_IN);
+                }
+            }
+        };
+
     }
 
     @Override
@@ -160,9 +225,8 @@
                 public void onCancelled(DatabaseError databaseError) {
 
                 }
-            }
-
-
+            };
             mDataBaseReference.addChildEventListener(mChildEventListener);
         }
     }
+}
